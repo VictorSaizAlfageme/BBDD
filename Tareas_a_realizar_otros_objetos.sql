a46d757c7604658e0d62b31dd2cef0a578cc43a7
victor <victor.saiz@ikasle.egibide.org>
2020-02-10 10:24:32 +0100

    Pepi

--- Tareas_a_realizar_otros_objetos.sql
+++ Tareas_a_realizar_otros_objetos.sql
@@ -12,7 +12,7 @@ select * from departseq;
 
 --Ejercicio 2
 
-create sequence DEP_IP_SEQ
+create sequence DEPT_ID_SEQ
 start with 200
 maxvalue 1000
 increment by 10
@@ -22,4 +22,31 @@ desc user_sequences;
 
 select sequence_name, min_value, max_value, increment_by
 from user_sequences
-where lower(sequence_name) = 'dep_ip_seq';
+where lower(sequence_name) = 'dept_id_seq';
+
+--Ejercicio 3
+
+select sequence_name, max_value, increment_by, last_number
+from user_sequences;
+
+--Ejercicio 4
+
+--está en el script lab9_4.sql
+
+--Ejercicio 5
+select dept_id_seq.nextval
+from dual;
+
+--Añadir un departamento con la secuandia recien generada
+insert into departseq values
+(dept_id_seq.currval, 'LOQUEQUIERAS', 'CORONAVIRUS');
+
+select *
+from departseq
+where dnombre = 'LOQUEQUIERAS';
+
+COMMIT;
+
+
+
+
